30685726b32e8f323e9d9abef4e80eaa54a51ab7
Adepo <hadade11@gmail.com>
2020-10-19 12:05:10 +0100

    Login

--- src/main/java/com/ayoola/hospital/Security/SecurityConfig.java
+++ src/main/java/com/ayoola/hospital/Security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception{
         web
                 .ignoring()
-                .antMatchers("/static/**", "/css/**", "/js/**", "img/**", "scss/**", "fonts/**");
+                .antMatchers("/static/**", "/css/**", "fonts/**", "img/**", "/js/**", "scss/**");
     }
 
     @Bean
